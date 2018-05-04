55fd31afb368bbc73e301cc0525615fcc0098bd0
James <jamesleahy314@gmail.com>
2018-05-03 22:47:34 -0700

    cors

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
 app.use('/user', user);
 app.use('/authentication', authentication);
 
-app.use(cors({credientials: true, origin: FRONTEND_ORIGIN}))
+app.use(cors({credientials: true, origin: false}))
 
 //LISTEN 
 app.listen(process.env.APP_PORT, function() {
